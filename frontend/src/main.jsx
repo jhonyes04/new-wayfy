b09e1340c7ab1ce9047437bc38ccb0e5503546b8
jhonyes04 <jhonyes@hotmail.com>
2026-06-23 13:28:50 +0200

    Cambio de bootstrap a paquetes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
 import { StoreProvider } from './context/store/StoreProvider';
 import { AuthProvider } from './context/auth/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import { ToastContainer } from 'react-toastify';
 
